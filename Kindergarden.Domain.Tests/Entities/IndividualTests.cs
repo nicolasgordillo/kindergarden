72d20207c4719e1d719817d49ab5a18b8547e6f0
Nicolas Gordillo <nicogordillo@gmail.com>
2019-06-20 13:28:37 -0300

    Tests de ejemplo de clases de dominio

---
+++ Kindergarden.Domain.Tests/Entities/IndividualTests.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarden.Domain.Entities;
+using Kindergarden.Domain.Exceptions;
+using Kindergarden.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kindergarden.Domain.Tests.ValueObjects
+{
+    public class IndividualTests
+    {
+        [Fact]
+        public void ShouldReturnNotAuthorizedToSendMessages()
+        {
+            var individual = new Individual()
+            {
+                Id = 1,
+                FirstName = "Test FirstName",
+                LastName = "Test LastName"
+            };
+
+            Assert.False(individual.CanSendMessage());
+        }
+    }
+}
---
+++ Kindergarden.Domain.Tests/ValueObjects/EmailTests.cs
@@ -0,0 +1,28 @@
+﻿using Kindergarden.Domain.Exceptions;
+using Kindergarden.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kindergarden.Domain.Tests.ValueObjects
+{
+    public class EmailTests
+    {
+        [Fact]
+        public void ToStringReturnsCorrectFormat()
+        {
+            const string value = "email@domain.com";
+
+            var email = Email.For(value);
+
+            Assert.Equal(value, email.ToString());
+        }
+
+        [Fact]
+        public void ShouldThrowEmailInvalidExceptionForInvalidEmail()
+        {
+            Assert.Throws<EmailInvalidException>(() => Email.For("email"));
+        }
+    }
+}
